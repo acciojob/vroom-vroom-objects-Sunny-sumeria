10d30d28a737dc0650f6d4e1a16a5c8f1915bf62
Sunny Sumeria <130832961+Sunny-sumeria@users.noreply.github.com>
2026-03-31 16:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-// Complete the js code
-function Car(make, model) {
-	this.make= make;
-	this.model = model;
-};
+// // Complete the js code
+// function Car(make, model) {
+// 	this.make= make;
+// 	this.model = model;
+// };
 
-car.prototype.getMakeModel = function() {
-	return `${this.make} ${this.model}`
-}
+// car.prototype.getMakeModel = function() {
+// 	return `${this.make} ${this.model}`
+// }
 
-function SportsCar(make, model, topSpeed) {
-	this.car(this,make,model);
-	this.topSpeed = topSpeed;
-}
+// function SportsCar(make, model, topSpeed) {
+// 	this.car(this,make,model);
+// 	this.topSpeed = topSpeed;
+// }
 
-SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
+// SportsCar.prototype=Object.create(Car.prototype);
+// SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = function () {
-	return this.topSpeed;
-};
+// SportsCar.prototype.getTopSpeed = function () {
+// 	return this.topSpeed;
+// };
 
 // Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+//window.Car = Car;
+//window.SportsCar = SportsCar;
+
+
+
+// 1. Car Constructor
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
+
+// Add method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return this.make + " " + this.model;
+};
+
+// 2. SportsCar Constructor
+function SportsCar(make, model, topSpeed) {
+    // Inherit properties from Car using .call()
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+// Link SportsCar prototype to Car prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset the constructor property (otherwise it points to Car)
+SportsCar.prototype.constructor = SportsCar;
+
+// Add method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+// Example Usage:
+const myCar = new SportsCar("Ferrari", "488 GTB", 205);
+console.log(myCar.getMakeModel()); // Output: Ferrari 488 GTB
+console.log(myCar.getTopSpeed());  // Output: 205
